9f4fc2400ac29bf13470804cf8d4d04d83b33216
Alex Elkhatib <alexanderelkhatib@yahoo.com>
2019-03-27 21:32:09 -0500

    pushing updates thus far

--- liri.js
+++ liri.js
@@ -2,6 +2,42 @@
 //Install Packages
 
 
-// require('dotenv').config();
+require('dotenv').config();
 
-// var keys = require("./keys.js")
+// Variables //
+
+var keys = require("./keys.js")
+var spotify = new Spotify(keys.spotify);
+var request = require("request");
+var fs = require("fs");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+
+// Input from user //
+var userAction = process.argv[2];
+var requestParameter = process.argv.slice(3).join(" ");
+
+// Function to carry out functions //
+UserInputs(userOption, inputParameter);
+
+// Functions //
+function UserWorking (userAction, requestParameter){
+    switch (userAction) {
+    case 'concert-this':
+        showConcertInfo(requestParameter);
+        break;
+    case 'spotify-this-song':
+        showSongInfo(requestParameter);
+        break;
+    case 'movie-this':
+        showMovieInfo(requestParameter);
+        break;
+    case 'do-what-it-says':
+        showSomeInfo();
+        break;
+    default: 
+        console.log("Sorry, we can't accept that input. Try again with any of theese options: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says")
+    }
+}
+
+// Music Response Function //
